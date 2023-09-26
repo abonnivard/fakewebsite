6890505b657c48aa0bea9e7e2286ba1d7c7c4001
adrien <adri.bonnivard@gmail.coml>
2023-09-25 22:39:00 -0400

    initial commit

--- Poly/wsgi.py
+++ Poly/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'formulaire.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Poly.settings')
 
 application = get_wsgi_application()
